2b96e6ffab2250c3ae08bd7d60cc575440b868dc
tnwong <wong.tsznok@gmail.com>
2020-07-17 17:35:39 +0800

    feat: session authentication

--- db/schema.rb
+++ db/schema.rb
@@ -10,13 +10,15 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2020_07_17_084907) do
+ActiveRecord::Schema.define(version: 2020_07_17_092513) do
 
   create_table "links", force: :cascade do |t|
     t.string "url"
     t.text "description"
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
+    t.integer "user_id"
+    t.index ["user_id"], name: "index_links_on_user_id"
   end
 
   create_table "users", force: :cascade do |t|
@@ -27,4 +29,5 @@ ActiveRecord::Schema.define(version: 2020_07_17_084907) do
     t.datetime "updated_at", precision: 6, null: false
   end
 
+  add_foreign_key "links", "users"
 end
